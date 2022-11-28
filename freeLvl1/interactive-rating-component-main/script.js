e7cc7f2664a952e4b4ec0b03c9e032004f88f5bf
Pedro Alves <pedro.alves.hf@gmail.com>
2022-11-28 16:39:34 -0300

    finishing the second one

--- freeLvl1/interactive-rating-component-main/script.js
+++ freeLvl1/interactive-rating-component-main/script.js
@@ -1,14 +1,35 @@
 const submitBtn = document.querySelector(".btn-submit");
-const containerOptions = document.querySelector(".container-main-options");
+const containerOptions = document.querySelector(".container-main-op");
 const optionList = document.querySelectorAll(".options");
+const containerMainOne = document.querySelector(".container-main-main");
+const containerMainSec = document.querySelector(".container-2o-main-main");
+const containerMainSecSelected = document.querySelectorAll(".options");
+const containerSecondText = document.querySelector(".container-2o-main-main-choose-text");
 
-
-function mouseOver() {
-    containerOptions.style.backgrounColor = "black";
+let firstValue = 0;
+function addAndRemoveSelected(event) {
+    if(event.target.classList.contains("options")){
+        const selectedOption = document.querySelector(".selected");
+        if(selectedOption){
+            selectedOption.classList.remove('selected');
+        }
+        event.target.classList.toggle('selected');
+        firstValue = event.target.innerText;
+        }
 };
 
-optionList.addEventListener('mouseover', () => {
-    containerOptions.style.backgrounColor = "black";
-    containerOptions.style.cursor = pointer;
+function submitNote() {
+    containerMainOne.style.display = "none";
+    containerMainSec.style.display = "flex";
+    containerSecondText.innerText = "You have selected: " + firstValue + " out of 5";
+};
 
+containerOptions.addEventListener('click', (event) => {
+    if (event.target.classList.contains("options")){
+    submitBtn.toggleAttribute('disabled');
+    submitBtn.classList.remove('disabled');
+    };
 });
+
+containerOptions.addEventListener('click', addAndRemoveSelected);
+submitBtn.addEventListener('click', submitNote);
